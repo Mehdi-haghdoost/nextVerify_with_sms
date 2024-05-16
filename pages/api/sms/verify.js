2e8e9471699d420b5d5123394beaff6340c25933
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-05-16 17:12:48 +0330

    coding verify api

---
+++ pages/api/sms/verify.js
@@ -0,0 +1,28 @@
+import otpModel from '@/models/otp'
+
+const handler = async (req, res) => {
+
+    if (req.method !== 'POST') {
+        return false;
+    }
+
+    const { phone, code } = req.body;
+
+    const otp = await otpModel.findOne({ phone, code })
+
+    if (otp) {
+        const date = new Date()
+        const now = date.getTime();
+
+        if (top.expTime > now) {
+            return res.status(200).json({ message: 'code is correct :))' })
+        } else {
+            return res.status(410).json({ message: 'code is expired !! :))' })
+        }
+
+    } else {
+        return res.status(409).json({ message: 'code is not correct !!' })
+    }
+}
+
+export default handler;
